6ab693188d5e2f57aef9a51b4c543d491d1b00a0
mikeg <mikegeor@outlook.com.gr>
2020-12-17 23:47:43 +0200

    Added helper functions

---
+++ src/ContextualWebSearchClient/Helpers/Statics/StringStatics.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContextualWebSearchClient.Helpers.Statics
+{
+    public static class StringStatics
+    {
+        public static string GenerateUrlQueryFrom<T>(T paramObject)
+        {
+
+        }
+    }
+}
