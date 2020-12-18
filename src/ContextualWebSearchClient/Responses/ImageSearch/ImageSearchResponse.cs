0a75393dddbea01bd4afefacf1a718180de6a1e3
mikeg <mikegeor@outlook.com.gr>
2020-12-18 18:36:14 +0200

    Fixed the Reponses and added Status Property

--- src/ContextualWebSearchClient/Responses/ImageSearch/ImageSearchResponse.cs
+++ src/ContextualWebSearchClient/Responses/ImageSearch/ImageSearchResponse.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections.Generic;
 using System.Text.Json.Serialization;
 using ContextualWebSearchClient.Responses.Shared;
+using System.Net;
 
 namespace ContextualWebSearchClient.Responses.ImageSearch
 {
     public class ImageSearchResponse
     {
+        [JsonIgnore]
+        public HttpStatusCode Status { get; set; }
+
         [JsonPropertyName("_type")]
         public string Type { get; set; }
         public int TotalCount { get; set; }
-        public List<SearchImage> Value { get; set; }
+
+        [JsonPropertyName("value")]
+        public List<SearchImage> Images { get; set; }
     }
 }
--- src/ContextualWebSearchClient/Responses/SpellCheck/SpellCheckResponse.cs
+++ src/ContextualWebSearchClient/Responses/SpellCheck/SpellCheckResponse.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Net;
+using System.Text.Json.Serialization;
 
 namespace ContextualWebSearchClient.Responses.SpellCheck
 {
     class SpellCheckResponse
     {
+        [JsonIgnore]
+        public HttpStatusCode Status { get; set; }
         public string Response { get; set; }
     }
 }
