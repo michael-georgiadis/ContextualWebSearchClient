bb68ce7b0a3761d88a65da008cdb84c80ca4381c
mikeg <mikegeor@outlook.com.gr>
2020-12-18 18:37:02 +0200

    Contextual News Search API Client now working

---
+++ src/ContextualWebSearchClient/ContextualNewsApiClient.cs
@@ -0,0 +1,59 @@
+﻿using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+using System;
+
+using ContextualWebSearchClient.Request.Url.Optional;
+using ContextualWebSearchClient.Request.Url.Required;
+using ContextualWebSearchClient.Responses.ImageSearch;
+using ContextualWebSearchClient.Responses.NewsAndWebSearch;
+using ContextualWebSearchClient.Helpers.Extensions;
+
+namespace ContextualWebSearchClient
+{
+    public class ContextualNewsApiClient : AbstractContextualClient
+    {
+        public ContextualNewsApiClient(string apiKey)
+            : base(apiKey) { }
+
+        public async Task<NewsAndWebSearchResponse> GetNews(SearchParameters searchParamaters, NewsOptionalParameters optionalParameters)
+        {
+            return await Get($"{searchParamaters}{optionalParameters}");
+        }
+
+        public void GetNews(SearchParameters searchParameters)
+        {
+
+        }
+
+        public async Task<NewsAndWebSearchResponse> GetNews(string query)
+        {
+            var parameters = new SearchParameters(query);
+
+            return await Get(parameters.ToString());
+        }
+
+        private async Task<NewsAndWebSearchResponse> Get(string parameters)
+        {
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = BuildUri("search", "NewsSearchAPI", parameters)
+            };
+
+            using (var response = await SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+
+                var responseBody = await response.Content.ReadAsStringAsync();
+
+                var newsResponse = JsonSerializer.Deserialize<NewsAndWebSearchResponse>(responseBody, serializationOptions);
+
+                newsResponse.Status = response.StatusCode;
+
+                return newsResponse;
+            }
+        }
+    }
+}
---
+++ src/ContextualWebSearchClient/ContextualWebSearchClient.cs
@@ -0,0 +1,31 @@
+﻿using ContextualWebSearchClient.Request.Url.Required;
+using ContextualWebSearchClient.Request.Url.Optional;
+
+namespace ContextualWebSearchClient
+{
+    public class ContextualWebSearchClient
+    {
+
+
+        public ContextualWebSearchClient(string apiKey) { }
+
+        public void NewsSearch()
+        {
+
+        }
+
+        public void ImageSearch()
+        {
+
+        }
+        public void WebSearch()
+        {
+
+        }
+
+        public void AutoComplete()
+        {
+
+        }
+    }
+}
