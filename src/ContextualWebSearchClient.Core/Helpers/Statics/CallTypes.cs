e73a464eb8abe29b23e42ea1667ac0968229a496
mikeg <mikegeor@outlook.com.gr>
2020-12-20 00:18:43 +0200

    Added static classes for helping in the URL building process

---
+++ src/ContextualWebSearchClient.Core/Helpers/Statics/CallTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContextualWebSearchClient.Core.Helpers.Statics
+{
+    public static class CallTypes
+    {
+        public const string Search = "search";
+        public const string Spelling = "spelling";
+    }
+}
---
+++ src/ContextualWebSearchClient.Core/Helpers/Statics/Controllers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContextualWebSearchClient.Core.Helpers.Statics
+{
+    public static class Controllers
+    {
+        public const string News = "NewsSearchAPI";
+        public const string Web = "WebSearchAPI";
+        public const string Image = "ImageSearchAPI";
+        public const string SpellCheck = "SpellCheck";
+        public const string AutoComplete = "AutoComplete";
+    }
+}
