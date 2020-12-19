6adc57e3e57a7a74696a0f12c94ca4cf759d0a42
mikeg <mikegeor@outlook.com.gr>
2020-12-20 00:20:32 +0200

    Added the data static classes that will be used for the tests

---
+++ tests/ContextualWebSearchClient.Tests/Data/NewsApiRequestData.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using ContextualWebSearchClient.Request.Url.Required;
+using ContextualWebSearchClient.Request.Url.Optional;
+using Xunit;
+
+namespace ContextualWebSearchClient.Tests.Data
+{
+    public static class NewsApiRequestData
+    {
+        public static TheoryData<string> SimpleQueryRequest =>
+            new TheoryData<string>
+            {
+                "Donald Trump",
+                "Joe Biden"
+            };
+
+        public static TheoryData<SearchParameters> RequiredParamatersRequest =>
+            new TheoryData<SearchParameters>
+            {
+                new SearchParameters("Donald Trump")
+                {
+                    AutoCorrect = false,
+                    PageNumber = 5
+                }
+            };
+
+        public static TheoryData<SearchParameters, NewsOptionalParameters> RequiredAndOptionalParametersRequest =>
+            new TheoryData<SearchParameters, NewsOptionalParameters>
+            {
+                {
+                    new SearchParameters("Donald Trump"),
+                    new NewsOptionalParameters { SafeSearch = false }
+                },
+                {
+                    new SearchParameters("Joe Biden")
+                    {
+                        PageSize = 10,
+                        AutoCorrect = false
+                    },
+                    new NewsOptionalParameters
+                    {
+                        FromPublishedDate = DateTime.Now.AddDays(-5)
+                    }
+                }
+            };
+    }
+}
