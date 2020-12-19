e5c591cbc5fc4e0c004833d740d2d3d64857a50c
mikeg <mikegeor@outlook.com.gr>
2020-12-20 00:20:51 +0200

    Added FluentAssertions and new test scenarios

--- tests/ContextualWebSearchClient.Tests/NewsApiClientTests.cs
+++ tests/ContextualWebSearchClient.Tests/NewsApiClientTests.cs
@@ -3,33 +3,68 @@ using Xunit;
 using ContextualWebSearchClient;
 using System.Threading.Tasks;
 using System.Net;
-using ContextualWebSearchClient.Tests.Data.NewsApi;
+using ContextualWebSearchClient.Tests.Data;
 using ContextualWebSearchClient.Request.Url.Required;
+using ContextualWebSearchClient.Request.Url.Optional;
+using FluentAssertions;
 
 namespace ContextualWebSearchClient.Tests
 {
     public class NewsApiClientTests : BaseApiClientTest
     {
-        private ContextualNewsApiClient _newApiClient;
+        private ContextualWebSearchClient _newApiClient;
 
 
         public NewsApiClientTests() : base() =>
 
-            _newApiClient = new ContextualNewsApiClient(Config["ContextualApiKey"]);
+            _newApiClient = new ContextualWebSearchClient(Config["ContextualApiKey"]);
 
 
         [Theory]
         [MemberData(nameof(NewsApiRequestData.SimpleQueryRequest), MemberType = typeof(NewsApiRequestData))]
         public async Task JustTextQuery(string query)
         {
-            var response = await _newApiClient.GetNews(query);
+            var response = await _newApiClient.NewsSearch(query);
+
+            response.Status
+                .Should()
+                .Be(HttpStatusCode.OK);
+
+
         }
 
         [Theory]
         [MemberData(nameof(NewsApiRequestData.RequiredParamatersRequest), MemberType = typeof(NewsApiRequestData))]
         public async Task RequiredParamatersQueryRequest(SearchParameters searchParamaters)
         {
-            var response = await _newApiClient.GetNews(searchParamaters);
+            var response = await _newApiClient.NewsSearch(searchParamaters);
+
+            response.Status
+                .Should()
+                .Be(HttpStatusCode.OK);
+
+            response.SearchResults
+                .Should()
+                .NotBeEmpty()
+                .And
+                .NotBeNull();
+        }
+
+        [Theory]
+        [MemberData(nameof(NewsApiRequestData.RequiredAndOptionalParametersRequest), MemberType = typeof(NewsApiRequestData))]
+        public async Task RequiredAndOptionalParametersQueryRequest(SearchParameters searchParameters, NewsOptionalParameters optionalParameters)
+        {
+            var response = await _newApiClient.NewsSearch(searchParameters, optionalParameters);
+
+            response.Status
+                .Should()
+                .Be(HttpStatusCode.OK);
+
+            response.SearchResults
+                .Should()
+                .NotBeEmpty()
+                .And
+                .NotBeNull();
         }
 
 
